94da4de80a72e938c5a4eebe9307f33de7509f1a
idkNicks <yangdaeyeong0808@gmail.com>
2023-07-03 10:46:59 +0900

    fix: implementation -> compilyOnly

--- Test-Plugin/src/main/java/kr/starly/testplugin/listener/PlayerInteractListener.java
+++ Test-Plugin/src/main/java/kr/starly/testplugin/listener/PlayerInteractListener.java
@@ -1,21 +1,20 @@
 package kr.starly.testplugin.listener;
 
-import kr.starly.core.StarlyCore;
+import kr.starly.core.util.ItemStackNameUtil;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class PlayerInteractListener implements Listener {
 
     @EventHandler
     public void onInteract(PlayerInteractEvent event) {
         Player player = event.getPlayer();
-        System.out.println(StarlyCore.getInstance().getName());
-//        ItemStack itemInMainHand = player.getInventory().getItemInMainHand();
-//
-//        String itemName = ItemStackNameUtil.getItemName(itemInMainHand);
-//
-//        System.out.println(itemName);
+        ItemStack itemInMainHand = player.getInventory().getItemInMainHand();
+        String itemName = ItemStackNameUtil.getItemName(itemInMainHand);
+
+        System.out.println(itemName);
     }
 }
