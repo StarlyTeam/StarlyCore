9aa269c7e3b0655ea80e60b4a745bd8ffa2deeab
idkNicks <yangdaeyeong0808@gmail.com>
2023-07-25 07:33:48 +0900

    Refactor ItemSerializeUtil to use Optional and enhance error logging.

--- src/main/java/kr/starly/core/util/ItemSerializeUtil.java
+++ src/main/java/kr/starly/core/util/ItemSerializeUtil.java
@@ -1,50 +1,41 @@
 package kr.starly.core.util;
 
+import org.bukkit.Bukkit;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.io.BukkitObjectInputStream;
 import org.bukkit.util.io.BukkitObjectOutputStream;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.util.Base64;
+import java.util.Optional;
 
 public class ItemSerializeUtil {
 
     private ItemSerializeUtil() {}
 
-    /**
-     * ItemStack을 직렬화하여 Base64로 인코딩합니다.
-     *
-     * @param itemStack 인코딩할 ItemStack 객체입니다.
-     * @return Base64로 인코딩된 직렬화된 문자열이 반환됩니다. 인코딩에 실패한 경우에는 null이 반환됩니다.
-     */
-    public static String encode(ItemStack itemStack) {
+    public static Optional<String> encode(ItemStack itemStack) {
         try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
              BukkitObjectOutputStream dataOutput = new BukkitObjectOutputStream(outputStream)) {
 
             dataOutput.writeObject(itemStack);
-            return Base64.getEncoder().encodeToString(outputStream.toByteArray());
-        } catch (IOException e) {
+            return Optional.of(Base64.getEncoder().encodeToString(outputStream.toByteArray()));
+        } catch (Exception e) {
+            Bukkit.getLogger().severe("An error occurred in ItemSerializeUtil: " + e.getMessage());
             e.printStackTrace();
-            return null;
+            return Optional.empty();
         }
     }
 
-    /**
-     * Base64로 인코딩된 직렬화된 ItemStack을 디코드합니다.
-     *
-     * @param serializedItemStack Base64 형식으로 직렬화된 ItemStack입니다.
-     * @return 디코드된 ItemStack이 반환되며, 디코드에 실패한 경우에는 null이 반환됩니다.
-     */
-    public static ItemStack decode(String serializedItemStack) {
+    public static Optional<ItemStack> decode(String serializedItemStack) {
         try (ByteArrayInputStream inputStream = new ByteArrayInputStream(Base64.getDecoder().decode(serializedItemStack));
              BukkitObjectInputStream dataInput = new BukkitObjectInputStream(inputStream)) {
 
-            return (ItemStack) dataInput.readObject();
-        } catch (IOException | ClassNotFoundException e) {
+            return Optional.of((ItemStack) dataInput.readObject());
+        } catch (Exception e) {
+            Bukkit.getLogger().severe("An error occurred in ItemSerializeUtil: " + e.getMessage());
             e.printStackTrace();
-            return null;
+            return Optional.empty();
         }
     }
 }
