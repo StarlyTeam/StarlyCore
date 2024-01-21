a88f44d630c74b780c83636c008fda32bcfb2a68
idkNicks <yangdaeyeong0808@gmail.com>
2024-01-21 15:02:46 +0900

    feat: remove Optional

--- src/main/java/kr/starly/core/util/ItemSerializeUtil.java
+++ src/main/java/kr/starly/core/util/ItemSerializeUtil.java
@@ -8,34 +8,31 @@ import org.bukkit.util.io.BukkitObjectOutputStream;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.util.Base64;
-import java.util.Optional;
 
 public class ItemSerializeUtil {
 
     private ItemSerializeUtil() {}
 
-    public static Optional<String> encode(ItemStack itemStack) {
+    public static String encode(ItemStack itemStack) {
         try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
              BukkitObjectOutputStream dataOutput = new BukkitObjectOutputStream(outputStream)) {
 
             dataOutput.writeObject(itemStack);
-            return Optional.of(Base64.getEncoder().encodeToString(outputStream.toByteArray()));
+            return Base64.getEncoder().encodeToString(outputStream.toByteArray());
         } catch (Exception e) {
             Bukkit.getLogger().severe("An error occurred in ItemSerializeUtil: " + e.getMessage());
-            e.printStackTrace();
-            return Optional.empty();
+            return null;
         }
     }
 
-    public static Optional<ItemStack> decode(String serializedItemStack) {
+    public static ItemStack decode(String serializedItemStack) {
         try (ByteArrayInputStream inputStream = new ByteArrayInputStream(Base64.getDecoder().decode(serializedItemStack));
              BukkitObjectInputStream dataInput = new BukkitObjectInputStream(inputStream)) {
 
-            return Optional.of((ItemStack) dataInput.readObject());
+            return (ItemStack) dataInput.readObject();
         } catch (Exception e) {
             Bukkit.getLogger().severe("An error occurred in ItemSerializeUtil: " + e.getMessage());
-            e.printStackTrace();
-            return Optional.empty();
+            return null;
         }
     }
 }
